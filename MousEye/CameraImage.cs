90c2056e8e675d0f87ff6792f1ee9b7ebabc11d2
Andrzej <a.bydzicki@gmail.com>
2015-06-04 21:36:25 +0200

    Start projektu

---
+++ MousEye/CameraImage.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MousEye
+{
+    public class CameraImage : Image, IDisposable
+    {
+        public CameraDevice CameraDevice { get; private set; }
+
+        public CameraImage()
+        {
+            CameraDevice = new CameraDevice();
+            CameraDevice.BitmapReady += OnBitmapReady;
+        }
+
+        ~CameraImage()
+        {
+            // Finalizer calls Dispose(false)
+            Dispose(false);
+        }
+
+        private void OnBitmapReady(object sender, EventArgs e)
+        {
+            Source = CameraDevice.BitmapSource;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // free managed resources
+                if (CameraDevice != null)
+                {
+                    CameraDevice.Stop();
+                    CameraDevice.Dispose();
+                    CameraDevice = null;
+                }
+            }
+            // free native resources if there are any.
+        }
+
+        #region [ Dependency Properties ]
+
+        public float Framerate
+        {
+            get { return (float)GetValue(FramerateProperty); }
+            set { SetValue(FramerateProperty, value); }
+        }
+
+        public static readonly DependencyProperty FramerateProperty =
+            DependencyProperty.Register("Framerate", typeof(float), typeof(CameraImage),
+            new UIPropertyMetadata((float)15, (PropertyChangedCallback)delegate(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+            {
+                CameraImage typedSender = sender as CameraImage;
+                typedSender.CameraDevice.Framerate = (float)e.NewValue;
+            }));
+
+        public CLEyeCameraColorMode ColorMode
+        {
+            get { return (CLEyeCameraColorMode)GetValue(ColorModeProperty); }
+            set { SetValue(ColorModeProperty, value); }
+        }
+
+        public static readonly DependencyProperty ColorModeProperty =
+            DependencyProperty.Register("ColorMode", typeof(CLEyeCameraColorMode), typeof(CameraImage),
+            new UIPropertyMetadata(default(CLEyeCameraColorMode), (PropertyChangedCallback)delegate(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+            {
+                CameraImage typedSender = sender as CameraImage;
+                typedSender.CameraDevice.ColorMode = (CLEyeCameraColorMode)e.NewValue;
+            }));
+
+        public CLEyeCameraResolution Resolution
+        {
+            get { return (CLEyeCameraResolution)GetValue(ResolutionProperty); }
+            set { SetValue(ResolutionProperty, value); }
+        }
+
+        public static readonly DependencyProperty ResolutionProperty =
+            DependencyProperty.Register("Resolution", typeof(CLEyeCameraResolution), typeof(CameraImage),
+            new UIPropertyMetadata(default(CLEyeCameraResolution), (PropertyChangedCallback)delegate(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+            {
+                CameraImage typedSender = sender as CameraImage;
+                typedSender.CameraDevice.Resolution = (CLEyeCameraResolution)e.NewValue;
+            }));
+
+        #endregion [ Dependency Properties ]
+    }
+}
---
+++ MousEye/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Windows;
+
+namespace MousEye
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int numCameras = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += new RoutedEventHandler(MainWindow_Loaded);
+            this.Closing += new System.ComponentModel.CancelEventHandler(MainWindow_Closing);
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (numCameras >= 1)
+            {
+                cameraImage1.CameraDevice.Stop();
+                cameraImage1.CameraDevice.Destroy();
+            }
+            if (numCameras == 2)
+            {
+                cameraImage2.CameraDevice.Stop();
+                cameraImage2.CameraDevice.Destroy();
+            }
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Query for number of connected cameras
+            numCameras = CameraDevice.CameraCount;
+            if (numCameras == 0)
+            {
+                MessageBox.Show("Could not find any PS3Eye cameras!");
+                return;
+            }
+            output.Items.Add(string.Format("Found {0} CLEyeCamera devices", numCameras));
+            // Show camera's UUIDs
+            for (int i = 0; i < numCameras; i++)
+            {
+                output.Items.Add(string.Format("CLEyeCamera #{0} UUID: {1}", i + 1, CameraDevice.CameraUUID(i)));
+            }
+            // Create cameras, set some parameters and start capture
+            if (numCameras >= 1)
+            {
+                cameraImage1.CameraDevice.Create(CameraDevice.CameraUUID(0));
+                cameraImage1.CameraDevice.Zoom = -50;
+                cameraImage1.CameraDevice.Start();
+            }
+            if (numCameras == 2)
+            {
+                cameraImage2.CameraDevice.Create(CameraDevice.CameraUUID(1));
+                cameraImage2.CameraDevice.Rotation = 200;
+                cameraImage2.CameraDevice.Start();
+            }
+        }
+    }
+}
