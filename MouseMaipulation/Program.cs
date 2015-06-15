8f5fcd5d56fbcfb659a78a15dbee24bf9aa81783
ukidabek <ukidabek@gmail.com>
2015-06-15 16:28:21 +0200

    Testy.

---
+++ MouseMaipulation/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace MouseMaipulation
+{
+    internal class Program
+    {
+        [DllImport("user32.dll")]
+        static extern void mouse_event(int dwFlags, int dx, int dy,
+                              int dwData, int dwExtraInfo);
+
+        [Flags]
+        public enum MouseEventFlags
+        {
+            LEFTDOWN = 0x00000002,
+            LEFTUP = 0x00000004,
+            MIDDLEDOWN = 0x00000020,
+            MIDDLEUP = 0x00000040,
+            MOVE = 0x00000001,
+            ABSOLUTE = 0x00008000,
+            RIGHTDOWN = 0x00000008,
+            RIGHTUP = 0x00000010
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool SetCursorPos(int X, int Y);
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Cursor.Position.ToString());
+            Console.ReadKey();
+
+            SetCursorPos(1850, 1000);
+
+            Console.WriteLine(Cursor.Position.ToString());
+            Console.ReadKey();
+
+            Console.WriteLine(Cursor.Position.ToString());
+            mouse_event((int)MouseEventFlags.LEFTDOWN, 1850, 1000, 0, 0);
+            mouse_event((int)MouseEventFlags.LEFTUP, 1850, 1000, 0, 0);
+            mouse_event((int)MouseEventFlags.LEFTDOWN, 1850, 1000, 0, 0);
+            mouse_event((int)MouseEventFlags.LEFTUP, 1850, 1000, 0, 0);
+
+
+            Console.ReadKey();
+        }
+    }
+}
